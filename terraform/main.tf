7af8e4ecc81001b19ddd77486c0b5e02032603c0
mmdcloud <madmaxcloudpractice@gmail.com>
2025-10-15 09:27:09 +0530

    update: added lambda and glue modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ module "raw_bucket" {
 # ----------------------------------------------------------------------
 # Curated Bucket
 # ----------------------------------------------------------------------
-module "raw_bucket" {
+module "curated_bucket" {
   source        = "./modules/s3"
   bucket_name   = "curated-bucket"
   objects       = []
@@ -53,17 +53,51 @@ module "raw_bucket" {
 # ----------------------------------------------------------------------
 # Lambda function ( for transformation )
 # ----------------------------------------------------------------------
-module "carshub_media_update_function" {
+module "lambda_function_role" {
+  source             = "../../../modules/iam"
+  role_name          = "lambda-function-role"
+  role_description   = "IAM role for transformation lambda function"
+  policy_name        = "lambda-function-role-policy"
+  policy_description = "IAM policy for transformation lambda function"
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                  "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }
+    EOF
+  policy             = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": [
+                  "logs:CreateLogGroup",
+                  "logs:CreateLogStream",
+                  "logs:PutLogEvents"
+                ],
+                "Resource": "arn:aws:logs:*:*:*",
+                "Effect": "Allow"
+            }
+        ]
+    }
+    EOF
+}
+
+module "lambda_function" {
   source        = "./modules/lambda"
   function_name = "serverless-transformation-function"
-  role_arn      = module.carshub_media_update_function_iam_role.arn
+  role_arn      = module.lambda_function_role.arn
   permissions   = []
-  env_variables = {
-    SECRET_NAME = module.carshub_db_credentials.name
-    DB_HOST     = tostring(split(":", module.carshub_db.endpoint)[0])
-    DB_NAME     = var.db_name
-    REGION      = var.region
-  }
+  env_variables = {}
   handler                 = "lambda.lambda_handler"
   runtime                 = "python3.12"
   s3_bucket               = module.carshub_media_update_function_code.bucket
@@ -71,3 +105,30 @@ module "carshub_media_update_function" {
   layers                  = [aws_lambda_layer_version.python_layer.arn]
   code_signing_config_arn = module.carshub_signing_profile.config_arn
 }
+
+# ----------------------------------------------------------------------
+# Glue configuration ( Crawler & Data catalog)
+# ----------------------------------------------------------------------
+resource "aws_glue_catalog_database" "database" {
+  name        = var.glue_database_name
+  description = "Glue database for incremental load"
+}
+
+resource "aws_glue_catalog_table" "table" {
+  name          = var.glue_table_name
+  database_name = aws_glue_catalog_database.database.name
+}
+
+resource "aws_glue_crawler" "crawler" {
+  database_name = aws_glue_catalog_database.database.name
+  name          = var.glue_crawler_name
+  role          = aws_iam_role.glue_crawler_role.arn
+
+  s3_target {
+    path = "s3://${module.source_bucket.bucket}"
+  }
+}
+
+# ----------------------------------------------------------------------
+# Athena configuration
+# ----------------------------------------------------------------------
